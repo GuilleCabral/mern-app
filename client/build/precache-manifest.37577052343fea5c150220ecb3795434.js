d2994cd3c5a68a876e63255e4a613d4b38569237
Guillermo Cabral <guille.cabral87@gmail.com>
2021-08-23 18:03:10 -0400

    some message

---
+++ client/build/precache-manifest.37577052343fea5c150220ecb3795434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3fc8327d3e29f3fcae5a9401372627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "967b8594ada6fba9263c",
+    "url": "/static/css/main.879909f4.chunk.css"
+  },
+  {
+    "revision": "a18efbcdfb4914b1d1f4",
+    "url": "/static/js/2.5741311c.chunk.js"
+  },
+  {
+    "revision": "967b8594ada6fba9263c",
+    "url": "/static/js/main.6104a87f.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
